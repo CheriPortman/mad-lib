b973f40e2fbf525ff8878fc90663b715c4aeea8f
Cheri Portman <cheriportman@hotmail.com>
2019-01-18 13:48:23 -0800

    refactored function

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 
 function makeMadLib () {
     
-    var noun1Input = document.getElementById('noun-1');
-    var noun2Input = document.getElementById('noun-2');
-    var verbInput = document.getElementById('verb');
-    var adjectiveInput = document.getElementById('adjective');
-    var pluralNounInput = document.getElementById('plural-noun');
+    // var noun1Input = document.getElementById('noun-1');
 
-    var noun1 = noun1Input.value;
-    var noun2 = noun2Input.value;
-    var verb = verbInput.value;
-    var adjective = adjectiveInput.value;
-    var pluralNoun = pluralNounInput.value;
+    var noun1 = document.getElementById('noun-1').value;
+    var noun2 = document.getElementById('noun-2').value;
+    var verb = document.getElementById('verb').value;
+    var adjective = document.getElementById('adjective').value;
+    var pluralNoun = document.getElementById('plural-noun').value;
 
-    var madText = 'The other day I rode my ' + noun1 + ' to the ' + noun2 + ' to ' + (verb) + ' some ' + adjective + ' ' + pluralNoun + ' .'
+    var madText = 'The other day I rode my ' + noun1 + ' to the ' 
+        + noun2 + ' to ' + (verb) + ' some ' + adjective + ' ' + pluralNoun + ' .'
 
     var madDisplay = document.getElementById('mad-lib');
     madDisplay.textContent = madText;
 }
 
 
+makeMadLib();
